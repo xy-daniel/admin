f3c2893c944c44a700c25653c1bbb5f995c75228
p4m4p <myarctic@163.com>
2020-03-28 16:39:49 +0800

    可视化界面的服务端

--- src/main/java/cn/cwyuan/admin/AdminApplication.java
+++ src/main/java/cn/cwyuan/admin/AdminApplication.java
@@ -1,9 +1,11 @@
 package cn.cwyuan.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AdminApplication {
 
     public static void main(String[] args) {
